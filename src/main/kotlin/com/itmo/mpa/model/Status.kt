346d3274cca390f72805338317820465c2721246
mbobova <mbobova@devexperts.com>
2019-04-01 10:02:16 +0300

    edit mappind for patient

--- src/main/kotlin/com/itmo/mpa/model/Status.kt
+++
@@ -1,5 +0,0 @@
-package com.itmo.mpa.model
-
-class Status(val status: String) {
-    var id: Int? = null
-}
--- src/main/kotlin/com/itmo/mpa/service/mapping/PatientDtoMapper.kt
+++ src/main/kotlin/com/itmo/mpa/service/mapping/PatientDtoMapper.kt
@@ -4,10 +4,10 @@ import com.itmo.mpa.dto.request.PatientRequest
 import com.itmo.mpa.dto.response.PatientResponse
 import com.itmo.mpa.entity.Patient
 
-fun PatientRequest.toModel() = Patient().apply {
-    this.name = name
-    this.age = age
-    this.status = status
+fun PatientRequest.toModel() = Patient().let {
+    it.age = age!!
+    it.name = name!!
+    it.status = status
 }
 
 fun Patient.toDto() = PatientResponse(0, name)
