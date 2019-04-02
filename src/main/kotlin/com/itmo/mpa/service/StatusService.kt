f2b8dbc07de8f667903b69780f9abd630b67af36
Georgy Pevnev <steyrixio@gmail.com>
2019-04-03 01:41:50 +0300

    implemented status service's functionality for draft managing

--- src/main/kotlin/com/itmo/mpa/service/StatusService.kt
+++ src/main/kotlin/com/itmo/mpa/service/StatusService.kt
@@ -1,12 +1,16 @@
 package com.itmo.mpa.service
 
+import com.itmo.mpa.dto.request.DraftRequest
+import com.itmo.mpa.dto.request.PatientRequest
 import com.itmo.mpa.dto.request.StatusRequest
 import com.itmo.mpa.dto.response.DraftResponse
 import com.itmo.mpa.dto.response.StatusResponse
 
 interface StatusService {
 
-    fun createStatus(statusRequest: StatusRequest)
+    fun commitDraft(id: Int)
+
+    fun createDraft(id: Int, draftRequest: DraftRequest)
 
     fun findStatus(id: Int) : StatusResponse?
 
--- src/main/kotlin/com/itmo/mpa/service/StatusServiceImpl.kt
+++ src/main/kotlin/com/itmo/mpa/service/StatusServiceImpl.kt
@@ -1,27 +1,38 @@
 package com.itmo.mpa.service
 
-import com.itmo.mpa.dto.request.StatusRequest
+import com.itmo.mpa.dto.request.DraftRequest
 import com.itmo.mpa.dto.response.DraftResponse
 import com.itmo.mpa.dto.response.StatusResponse
+import com.itmo.mpa.model.Status
+import com.itmo.mpa.repository.DraftRepository
 import com.itmo.mpa.repository.PatientRepository
 import com.itmo.mpa.service.mapping.toDto
+import com.itmo.mpa.service.mapping.toModel
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class StatusServiceImpl(private val patientRepository: PatientRepository) : StatusService {
+class StatusServiceImpl(private val patientRepository: PatientRepository, private val draftRepository: DraftRepository) : StatusService {
 
-    override fun createStatus(statusRequest: StatusRequest) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun commitDraft(id: Int) {
+        val draft = draftRepository.findById(id).orElseThrow { NotFoundException("Draft for patient with id $id not found")  }
+        patientRepository.findById(id).get().status = Status(draft.draft)
+    }
+
+
+    override fun createDraft(id: Int, draftRequest: DraftRequest) {
+        val draft = draftRequest.toModel()
+        draft.id = id
+
+        draftRepository.save(draft)
     }
 
     override fun findStatus(id: Int): StatusResponse? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val patient = patientRepository.findById(id).orElseThrow { NotFoundException("Patient $id not found") }
+        return Optional.ofNullable(patient.status).map { it.toDto() }.orElse(null)
     }
 
     override fun findStatusDraft(id: Int): DraftResponse? {
-        val patient = patientRepository.findById(id).orElseThrow { NotFoundException("Patient $id not found") }
-        val status = patient.status
-        return Optional.ofNullable(status.draft).map { it.toDto() }.orElse(null)
+        return draftRepository.findById(id).map { it.toDto() }.orElse(null)
     }
 }
