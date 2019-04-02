5970b9dc07d678573c44fe041938bbda0126cecd
dmrsmglov <damiron123@gmail.com>
2019-04-02 18:06:54 +0300

    preparation bracket computing

--- src/main/kotlin/com/itmo/mpa/service/parsing/Parser.kt
+++ src/main/kotlin/com/itmo/mpa/service/parsing/Parser.kt
@@ -6,8 +6,29 @@ import org.springframework.stereotype.Component
 class Parser {
 
     fun parse(expression: String) : Boolean {
-        //todo not implemented
-        return true
+        val openBracketsOnPrefix = ArrayList<Int>()
+        val closeBracketsOnSuffix = ArrayList<Int>()
+
+        var openBrackets = 0
+        var closeBrackets = 0
+
+        for (character in expression) {
+            if (character == '(') {
+                openBracketsOnPrefix.add(++openBrackets)
+            } else {
+                openBracketsOnPrefix.add(openBrackets)
+            }
+        }
+
+        for (character in expression) {
+            if (character == ')') {
+                closeBracketsOnSuffix.add(++closeBrackets)
+            } else {
+                closeBracketsOnSuffix.add(openBrackets - closeBrackets++)
+            }
+        }
+
+        return parse(expression, 0, expression.length, openBracketsOnPrefix, closeBracketsOnSuffix)
     }
 
     private fun parse(expression: String,
