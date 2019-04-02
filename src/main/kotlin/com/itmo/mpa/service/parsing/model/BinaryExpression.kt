47ec279a05a4919d09966ce2a6232e731df77d87
AntipovAndrey <antipov11.11@gmail.com>
2019-04-02 22:08:11 +0300

    parser: add BinaryExpression evaluator

--- src/main/kotlin/com/itmo/mpa/service/parsing/model/BinaryExpression.kt
+++ src/main/kotlin/com/itmo/mpa/service/parsing/model/BinaryExpression.kt
@@ -16,3 +16,22 @@ class Evaluated<T : Comparable<T>>(val value: Boolean) : BinaryExpression<T>()
 sealed class Value<T>
 class UnknownValue<T : Comparable<T>>(val number: Int) : Value<T>()
 class KnownValue<T : Comparable<T>>(val value: T) : Value<T>()
+
+fun <T : Comparable<T>> BinaryExpression<T>.evaluate(parameters: List<T>): Boolean {
+    return when (this) {
+        is Equal -> left.calculate(parameters) == right.calculate(parameters)
+        is LessThan -> left.calculate(parameters) < right.calculate(parameters)
+        is GreaterThan -> left.calculate(parameters) > right.calculate(parameters)
+        is Not -> !other.evaluate(parameters)
+        is Or -> left.evaluate(parameters) || right.evaluate(parameters)
+        is And -> left.evaluate(parameters) && right.evaluate(parameters)
+        is Evaluated -> value
+    }
+}
+
+fun <T : Comparable<T>> Value<T>.calculate(parameters: List<T>): T {
+    return when (this) {
+        is KnownValue -> value
+        is UnknownValue -> runCatching { parameters[number] }.getOrElse { throw ArgumentsCountMismatchException(parameters, it) }
+    }
+}
