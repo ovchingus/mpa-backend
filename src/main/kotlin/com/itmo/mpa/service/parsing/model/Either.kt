26b1033e896a6e8594a03f43efce84ad06b7e0fc
AntipovAndrey <antipov11.11@gmail.com>
2019-04-02 21:18:45 +0300

    parser: add BinaryExpression with Either support

--- src/main/kotlin/com/itmo/mpa/service/parsing/model/Either.kt
+++ src/main/kotlin/com/itmo/mpa/service/parsing/model/Either.kt
@@ -1,9 +1,44 @@
 package com.itmo.mpa.service.parsing.model
 
-sealed class Either<out L : Comparable<*>, out R : Comparable<*>>
+sealed class Either<L : Comparable<L>, R : Comparable<R>> : Comparable<Either<L, R>> {
 
-class Numerical<out T>(val value: T) : Either<T, Nothing>()
-        where T : Comparable<*>, T : Number
+    override operator fun compareTo(other: Either<L, R>): Int {
+        return when (this) {
+            is Numerical -> {
+                when (other) {
+                    is Numerical -> this.value.compareTo(other.value)
+                    is Textual -> this.value.toString().compareTo(other.value.toString())
+                }
+            }
+            is Textual -> other.compareTo(this)
+        }
+    }
 
-class Textual<out T>(val value: T) : Either<Nothing, T>()
-        where T : Comparable<*>, T : CharSequence
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Either<*, *>) return false
+
+        return when (this) {
+            is Numerical -> {
+                when (other) {
+                    is Numerical<*> -> this.value == other.value
+                    is Textual<*> -> this.value.toString() == other.value.toString()
+                }
+            }
+            is Textual -> other == this
+        }
+    }
+
+    override fun hashCode(): Int {
+        return when (this) {
+            is Numerical -> this.value.hashCode()
+            is Textual -> this.value.hashCode()
+        }
+    }
+}
+
+class Numerical<T>(val value: T) : Either<T, Nothing>()
+        where T : Comparable<T>, T : Number
+
+class Textual<T>(val value: T) : Either<Nothing, T>()
+        where T : Comparable<T>, T : CharSequence
