96677930c0a7144920a74ffe240c41683266f977
ehlinashin <ehlinashin@yandex.ru>
2019-04-06 19:44:59 +0300

    Exception handler added

---
+++ src/main/kotlin/com/itmo/mpa/controller/RestBaseExceptionHandler.kt
@@ -0,0 +1,42 @@
+package com.itmo.mpa.controller
+
+import com.itmo.mpa.service.NotFoundException
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+
+@ControllerAdvice
+class RestBaseExceptionHandler : ResponseEntityExceptionHandler() {
+    data class Error(val message: String?, val status: Int)
+
+
+    @ExceptionHandler(NotFoundException::class)
+    fun handleException(exception: NotFoundException, request: WebRequest): ResponseEntity<Any> {
+
+        val status = HttpStatus.NOT_FOUND
+        return handleExceptionInternal(exception,
+                Error(exception.message, status.value()),
+                HttpHeaders(),
+                status,
+                request)
+    }
+
+    /**
+     * Handle validation errors ([javax.validation.Valid]). The original method suppressed exception message
+     */
+    override fun handleMethodArgumentNotValid(exception: MethodArgumentNotValidException,
+                                              headers: HttpHeaders, status: HttpStatus,
+                                              request: WebRequest): ResponseEntity<Any> {
+
+        return handleExceptionInternal(exception,
+                Error(exception.message, status.value()),
+                HttpHeaders(),
+                status,
+                request)
+    }
+}
