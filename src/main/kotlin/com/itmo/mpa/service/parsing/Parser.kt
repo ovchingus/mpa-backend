ba0c11a536e7f0755958606827187c51ca7a7382
dmrsmglov <damiron123@gmail.com>
2019-04-09 23:14:24 +0300

    parser: create function for parsing values and change algorithm for find delimiter in expression

--- src/main/kotlin/com/itmo/mpa/service/parsing/Parser.kt
+++ src/main/kotlin/com/itmo/mpa/service/parsing/Parser.kt
@@ -1,7 +1,7 @@
 package com.itmo.mpa.service.parsing
 
-import com.itmo.mpa.service.parsing.model.Either
-import com.sun.javafx.fxml.expression.BinaryExpression
+import com.itmo.mpa.service.parsing.model.*
+
 import org.springframework.stereotype.Component
 
 @Component
@@ -9,58 +9,42 @@ class Parser {
 
     private val supportedOperations = Operations.values().map { it.token }
 
-    fun parse(expression: String): (List<Either<Int, String>>) -> Boolean {
-        val openBracketsOnPrefix = ArrayList<Int>()
-        val closeBracketsOnSuffix = ArrayList<Int>()
-
-        var openBrackets = 0
-        var closeBrackets = 0
-
-        for (character in expression) {
-            if (character == '(') {
-                openBracketsOnPrefix.add(++openBrackets)
-            } else {
-                openBracketsOnPrefix.add(openBrackets)
-            }
-        }
-
-        for (character in expression) {
-            if (character == ')') {
-                closeBracketsOnSuffix.add(++closeBrackets)
-            } else {
-                closeBracketsOnSuffix.add(openBrackets - closeBrackets++)
-            }
-        }
-
-        return parse(expression, 0, expression.length, openBracketsOnPrefix, closeBracketsOnSuffix)
+    fun parse(expression: String): BinaryExpression<Either<Double, String>> {
+        val trimmedExpression = expression.filterNot { it -> it == ' ' }
+        return parseExpression(trimmedExpression, 0, trimmedExpression.length)
     }
 
-    private fun parse(expression: String,
-                      leading: Int,
-                      following: Int,
-                      openBracketsOnPrefix: List<Int>,
-                      closeBracketsOnSuffix: List<Int>): (List<Either<Int, String>>) -> Boolean {
-        var resultExpression: Nothing?
+    private fun parseExpression(expression: String,
+                                leading: Int,
+                                following: Int): BinaryExpression<Either<Double, String>> {
+
+        val resultExpression: BinaryExpression<Either<Double, String>>?
         var head = leading
         var exprCandidate = ""
         while (true) {
             exprCandidate += expression[head]
             if (supportedOperations.contains(exprCandidate)) {
                 resultExpression = when (exprCandidate) {
-                    Operations.NOT.token -> not(parse(expression, head + 2, following - 1, openBracketsOnPrefix, closeBracketsOnSuffix))
-                    Operations.AND.token, Operations.EQ.token, Operations.GT.token, Operations.LT.token, Operations.OR.token -> {
-                        val delimiter = partition(head + 2, following - 1, openBracketsOnPrefix, closeBracketsOnSuffix)
+                    Operations.NOT.token -> Not(parseExpression(expression, head + 2, following - 1))
+                    Operations.AND.token, Operations.OR.token -> {
+                        val delimiter = partition(expression, head + 2, following)
                         when (exprCandidate) {
-                            Operations.AND.token -> and(parse(expression, head + 2, delimiter, openBracketsOnPrefix, closeBracketsOnSuffix),
-                                    parse(expression, delimiter + 1, following, openBracketsOnPrefix, closeBracketsOnSuffix))
-                            Operations.OR.token -> or(parse(expression, head + 2, delimiter, openBracketsOnPrefix, closeBracketsOnSuffix),
-                                    parse(expression, delimiter + 1, following, openBracketsOnPrefix, closeBracketsOnSuffix))
-                            Operations.EQ.token -> eq(parse(expression, head + 2, delimiter, openBracketsOnPrefix, closeBracketsOnSuffix),
-                                    parse(expression, delimiter + 1, following, openBracketsOnPrefix, closeBracketsOnSuffix))
-                            Operations.GT.token -> gt(parse(expression, head + 2, delimiter, openBracketsOnPrefix, closeBracketsOnSuffix),
-                                    parse(expression, delimiter + 1, following, openBracketsOnPrefix, closeBracketsOnSuffix))
-                            Operations.LT.token -> lt(parse(expression, head + 2, delimiter, openBracketsOnPrefix, closeBracketsOnSuffix),
-                                    parse(expression, delimiter + 1, following, openBracketsOnPrefix, closeBracketsOnSuffix))
+                            Operations.AND.token -> And(parseExpression(expression, head + 2, delimiter + 1),
+                                    parseExpression(expression, delimiter + 2, following - 1))
+                            Operations.OR.token -> Or(parseExpression(expression, head + 2, delimiter + 1),
+                                    parseExpression(expression, delimiter + 2, following - 1))
+                            else -> null
+                        }
+                    }
+                    Operations.EQ.token, Operations.GT.token, Operations.LT.token -> {
+                        val delimiter = expression.indexOf(',', head + 2, false)
+                        when (exprCandidate) {
+                            Operations.EQ.token -> Equal(parseValue(expression, head + 2, delimiter),
+                                    parseValue(expression, delimiter + 1, following - 1))
+                            Operations.GT.token -> GreaterThan(parseValue(expression, head + 2, delimiter),
+                                    parseValue(expression, delimiter + 1, following - 1))
+                            Operations.LT.token -> LessThan(parseValue(expression, head + 2, delimiter),
+                                    parseValue(expression, delimiter + 1, following - 1))
                             else -> null
                         }
                     }
@@ -73,22 +57,34 @@ class Parser {
         }
     }
 
+    private fun partition(expression: String, leading: Int, following: Int): Int {
 
-    fun partition(leading: Int,
-                  following: Int,
-                  openBracketsOnPrefix: List<Int>,
-                  closeBracketsOnSuffix: List<Int>): Int {
-        val x = openBracketsOnPrefix[openBracketsOnPrefix.size - 1] - openBracketsOnPrefix[leading]
-        var l = leading
-        var r = following
-        while (r - l > 1) {
-            val middle = (l + r) / 2
-            if (closeBracketsOnSuffix[middle] >= x) {
-                l = middle
-            } else {
-                r = middle
+        var balance = 0
+        var result = leading
+        for (i in leading..following) {
+            if (expression[i] == '(') {
+                balance++
+            } else if (expression[i] == ')') {
+                balance--
+            } else if (expression[i] == ',' && balance == 0) {
+                result = i - 1
+                break
             }
         }
-        return l
+        return result
+    }
+
+    private fun parseValue(expression: String,
+                           leading: Int,
+                           following: Int): Value<Either<Double, String>> {
+        if (expression[leading] == '$') {
+            return UnknownValue(expression.subSequence(leading + 1, following).toString().toInt())
+        } else {
+            return try {
+                KnownValue(Numerical(expression.subSequence(leading, following).toString().toDouble()))
+            } catch (ex: NumberFormatException) {
+                KnownValue(Textual(expression.subSequence(leading, following).toString()))
+            }
+        }
     }
 }
