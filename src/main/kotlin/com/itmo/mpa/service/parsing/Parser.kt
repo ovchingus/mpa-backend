80bca9eb20128f4ac481f175610e5bf0c1b7dd46
AntipovAndrey <antipov11.11@gmail.com>
2019-04-10 19:56:57 +0300

    parser: refactoring and exceptions

--- src/main/kotlin/com/itmo/mpa/service/parsing/Parser.kt
+++ src/main/kotlin/com/itmo/mpa/service/parsing/Parser.kt
@@ -21,34 +21,34 @@ class Parser {
             leading: Int = 0,
             following: Int = expression.length
     ): BinaryExpression<Either<Double, String>> {
-
         var head = leading
         var expressionCandidate = ""
 
         while (true) {
             expressionCandidate += expression[head]
 
-            if (supportedOperations.contains(expressionCandidate)) {
-                val operation = supportedOperations[expressionCandidate]!!
-
-                return when (operation.type) {
-                    UnaryOperation -> parseUnary(operation, expression, head, following)
-                    LogicalOperation -> parseLogical(operation, expression, head, following)
-                    ComparisonOperation -> parseComparison(operation, expression, head, following)
-                }
-            } else {
+            if (!supportedOperations.contains(expressionCandidate)) {
                 head++
+                if (head == expression.length) throw UnexpectedTokenException(expression)
+                continue
+            }
+
+            val operation = supportedOperations[expressionCandidate]!!
+            val arguments = ParsingArguments(operation, expression, head, following)
+            return when (operation.type) {
+                UnaryOperation -> parseUnary(arguments)
+                LogicalOperation -> parseLogical(arguments)
+                ComparisonOperation -> parseComparison(arguments)
             }
         }
     }
 
-    private fun parseComparison(
-            operation: Operation,
-            expression: String,
-            head: Int,
-            following: Int
-    ): BinaryExpression<Either<Double, String>> {
+    private fun parseComparison(args: ParsingArguments): BinaryExpression<Either<Double, String>> {
+        val (operation, expression, head, following) = args
+
         val delimiter = expression.indexOf(',', head + 2, false)
+        if (delimiter < 0) throw UnexpectedTokenException(expression)
+
         val left = parseValue(expression, head + 2, delimiter)
         val right = parseValue(expression, delimiter + 1, following - 1)
 
@@ -60,13 +60,12 @@ class Parser {
         }
     }
 
-    private fun parseLogical(
-            operation: Operation,
-            expression: String,
-            head: Int,
-            following: Int
-    ): BinaryExpression<Either<Double, String>> {
-        val delimiter = partition(expression, head + 2, following)
+    private fun parseLogical(args: ParsingArguments): BinaryExpression<Either<Double, String>> {
+        val (operation, expression, head, following) = args
+
+        val delimiter = partitionIndex(expression, head + 2, following)
+        if (delimiter > following) throw UnexpectedTokenException(expression)
+
         val left = parseExpression(expression, head + 2, delimiter + 1)
         val right = parseExpression(expression, delimiter + 2, following - 1)
 
@@ -77,25 +76,21 @@ class Parser {
         }
     }
 
-    private fun parseUnary(
-            operation: Operation,
-            expression: String,
-            head: Int,
-            following: Int
-    ): Not<Either<Double, String>> {
-        val argument = parseExpression(expression, head + 2, following - 1)
+    private fun parseUnary(args: ParsingArguments): BinaryExpression<Either<Double, String>> {
+        val (operation, expression, head, following) = args
 
+        val argument = parseExpression(expression, head + 2, following - 1)
         return when (operation) {
             Operation.NOT -> Not(argument)
             else -> throw UnexpectedTokenException(operation.token)
         }
     }
 
-    private fun partition(expression: String, leading: Int, following: Int): Int {
+    private fun partitionIndex(expression: String, leading: Int, following: Int): Int {
 
         var balance = 0
         var result = leading
-        for (i in leading..following) {
+        for (i in leading until following) {
             if (expression[i] == '(') {
                 balance++
             } else if (expression[i] == ')') {
@@ -108,17 +103,18 @@ class Parser {
         return result
     }
 
-    private fun parseValue(expression: String,
-                           leading: Int,
-                           following: Int): Value<Either<Double, String>> {
+    private fun parseValue(
+            expression: String,
+            leading: Int,
+            following: Int
+    ): Value<Either<Double, String>> {
         if (expression[leading] == '$') {
             return UnknownValue(expression.subSequence(leading + 1, following).toString().toInt())
-        } else {
-            return try {
-                KnownValue(Numerical(expression.subSequence(leading, following).toString().toDouble()))
-            } catch (ex: NumberFormatException) {
-                KnownValue(Textual(expression.subSequence(leading, following).toString()))
-            }
+        }
+        return try {
+            KnownValue(Numerical(expression.subSequence(leading, following).toString().toDouble()))
+        } catch (ex: NumberFormatException) {
+            KnownValue(Textual(expression.subSequence(leading, following).toString()))
         }
     }
 }
