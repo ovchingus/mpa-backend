b252a8f16d88afd3efc3a40bf2542116b9f506ac
AntipovAndrey <antipov11.11@gmail.com>
2019-04-10 19:27:43 +0300

    parser: split parsing by type of expression

--- src/main/kotlin/com/itmo/mpa/service/parsing/Parser.kt
+++ src/main/kotlin/com/itmo/mpa/service/parsing/Parser.kt
@@ -7,56 +7,87 @@ import org.springframework.stereotype.Component
 @Component
 class Parser {
 
-    private val supportedOperations = Operations.values()
+    private val supportedOperations = Operation.values()
             .groupBy { it.token }
             .mapValues { it.value.first() }
 
     fun parse(expression: String): BinaryExpression<Either<Double, String>> {
         val trimmedExpression = expression.filterNot { it -> it == ' ' }
-        return parseExpression(trimmedExpression, 0, trimmedExpression.length)
+        return parseExpression(trimmedExpression)
     }
 
-    private fun parseExpression(expression: String,
-                                leading: Int,
-                                following: Int): BinaryExpression<Either<Double, String>> {
+    private fun parseExpression(
+            expression: String,
+            leading: Int = 0,
+            following: Int = expression.length
+    ): BinaryExpression<Either<Double, String>> {
 
-        val resultExpression: BinaryExpression<Either<Double, String>>?
         var head = leading
         var expressionCandidate = ""
+
         while (true) {
             expressionCandidate += expression[head]
 
             if (supportedOperations.contains(expressionCandidate)) {
                 val operation = supportedOperations[expressionCandidate]!!
 
-                resultExpression = when (operation) {
-                    Operations.NOT -> Not(parseExpression(expression, head + 2, following - 1))
-                    Operations.AND, Operations.OR -> {
-                        val delimiter = partition(expression, head + 2, following)
-                        val left = parseExpression(expression, head + 2, delimiter + 1)
-                        val right = parseExpression(expression, delimiter + 2, following - 1)
-                        when (operation) {
-                            Operations.AND -> And(left, right)
-                            Operations.OR -> Or(left, right)
-                            else -> null
-                        }
-                    }
-                    Operations.EQ, Operations.GT, Operations.LT -> {
-                        val delimiter = expression.indexOf(',', head + 2, false)
-                        val left = parseValue(expression, head + 2, delimiter)
-                        val right = parseValue(expression, delimiter + 1, following - 1)
-                        when (operation) {
-                            Operations.EQ -> Equal(left, right)
-                            Operations.GT -> GreaterThan(left, right)
-                            Operations.LT -> LessThan(left, right)
-                            else -> null
-                        }
-                    }
+                return when (operation.type) {
+                    UnaryOperation -> parseUnary(operation, expression, head, following)
+                    LogicalOperation -> parseLogical(operation, expression, head, following)
+                    ComparisonOperation -> parseComparison(operation, expression, head, following)
                 }
-                return resultExpression!!
             } else {
                 head++
             }
+        }
+    }
+
+    private fun parseComparison(
+            operation: Operation,
+            expression: String,
+            head: Int,
+            following: Int
+    ): BinaryExpression<Either<Double, String>> {
+        val delimiter = expression.indexOf(',', head + 2, false)
+        val left = parseValue(expression, head + 2, delimiter)
+        val right = parseValue(expression, delimiter + 1, following - 1)
+
+        return when (operation) {
+            Operation.EQ -> Equal(left, right)
+            Operation.GT -> GreaterThan(left, right)
+            Operation.LT -> LessThan(left, right)
+            else -> throw UnexpectedTokenException(operation.token)
+        }
+    }
+
+    private fun parseLogical(
+            operation: Operation,
+            expression: String,
+            head: Int,
+            following: Int
+    ): BinaryExpression<Either<Double, String>> {
+        val delimiter = partition(expression, head + 2, following)
+        val left = parseExpression(expression, head + 2, delimiter + 1)
+        val right = parseExpression(expression, delimiter + 2, following - 1)
+
+        return when (operation) {
+            Operation.AND -> And(left, right)
+            Operation.OR -> Or(left, right)
+            else -> throw UnexpectedTokenException(operation.token)
+        }
+    }
+
+    private fun parseUnary(
+            operation: Operation,
+            expression: String,
+            head: Int,
+            following: Int
+    ): Not<Either<Double, String>> {
+        val argument = parseExpression(expression, head + 2, following - 1)
+
+        return when (operation) {
+            Operation.NOT -> Not(argument)
+            else -> throw UnexpectedTokenException(operation.token)
         }
     }
 
