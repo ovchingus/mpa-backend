62bfb5928b31c30c257d00915de250f3476767e6
AntipovAndrey <antipov11.11@gmail.com>
2019-05-22 21:26:45 +0300

    predicates: add collectReferences to BinaryExpression

---
+++ src/test/kotlin/com/itmo/mpa/service/impl/parsing/model/BinaryExpressionKtTest.kt
@@ -0,0 +1,26 @@
+package com.itmo.mpa.service.impl.parsing.model
+
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.containsInAnyOrder
+import org.junit.jupiter.api.Test
+
+class BinaryExpressionKtTest {
+
+    @Test
+    fun `test collecting symbolic names`() {
+        val predicate = Or(
+                And(
+                        Equal(KnownValue(""), UnknownValue("ref1")),
+                        GreaterThan(UnknownValue("ref2"), UnknownValue("ref1"))
+                ),
+                Not(
+                        And(
+                                LessThanEqual(UnknownValue("ref1"), UnknownValue("ref4")),
+                                LessThan(UnknownValue("ref3"), KnownValue(""))
+                        )
+                )
+        )
+        val references = predicate.collectReferences()
+        assertThat(references, containsInAnyOrder("ref1", "ref2", "ref3", "ref4"))
+    }
+}
