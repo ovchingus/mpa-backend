1cdf76196f0b8ffcd1db7d5707fc9a451b6c8ed3
woainikk <woainimrgrt@gmail.com>
2019-11-18 11:24:35 +0300

    edited paths for images and added new method for requesting state from db

--- src/main/kotlin/com/itmo/mpa/service/impl/entityservice/StateEntityService.kt
+++ src/main/kotlin/com/itmo/mpa/service/impl/entityservice/StateEntityService.kt
@@ -4,15 +4,22 @@ import com.itmo.mpa.entity.Disease
 import com.itmo.mpa.entity.states.State
 import com.itmo.mpa.repository.StateRepository
 import com.itmo.mpa.service.exception.NoInitialStateException
+import com.itmo.mpa.service.exception.StateNotFoundException
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
 class StateEntityService(
-    private val stateRepository: StateRepository
+        private val stateRepository: StateRepository
 ) {
 
     fun findInitialState(disease: Disease): State {
         return runCatching { stateRepository.findInitialState(disease) }.getOrNull()
                 ?: throw NoInitialStateException(disease.name)
     }
+
+    fun findImageForState(stateId: Long): State =
+            stateRepository.findByIdOrNull(stateId)
+                    ?: throw StateNotFoundException(stateId)
 }
+
